11f23cf42900b54dbd178bd4a563e46a4f945e66
Cody Abbott <cody.abbott16@gmail.com>
2020-03-27 17:09:34 +0000

    linked up project list with react store

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Notifications from "./Notifications";
 import ProjectList from "../projects/ProjectList";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Dashboard() {
+  const projects = useSelector(state => state.project.projects);
+  // console.log(projects);
+
   return (
     <div className="dashboard container">
       <div className="row">
         <div className="col s12 m6">
-          <ProjectList />
+          <ProjectList projects={projects} />
         </div>
         <div className="col s12 m5 offset-m1">
           <Notifications />
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ProjectSummary from "./ProjectSummary";
 
-export default function ProjectList() {
+export default function ProjectList({ projects }) {
   return (
     <div className="project-list section">
-      <ProjectSummary />
-      <ProjectSummary />
+      {projects &&
+        projects.map(project => {
+          return <ProjectSummary project={project} />;
+        })}
     </div>
   );
 }
+
+//the project && line means that it won't render until projects is true
+//aka projects exist
