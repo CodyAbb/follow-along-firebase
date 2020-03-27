655435736b6e260cddb740d7a1109ed162071b72
Cody Abbott <cody.abbott16@gmail.com>
2020-03-27 15:31:59 +0000

    intial redux setup with store and prject dummy data

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+  projects: [
+    { id: "1", title: "World map gam", body: "blah badfdfs dasf df" },
+    { id: "2", title: "World games fun", body: "blah badfdfs dasf df" },
+    { id: "3", title: "Rams", body: "blah badfdfs dasf df" }
+  ]
+};
+
+const projectReducer = (state = initState, action) => {
+  return state;
+};
+
+export default projectReducer;
